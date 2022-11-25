3e9ca51c90913f3e4ad06cfd15fd719b0448142d
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-11-25 12:06:02 +0100

    he creado la funcion huffmantree donde he almacenado en un diccionario la frecuencia y he creado una cola

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class arbol:
     def __init__(self,frecuencia,simbolo) :
         self.frecuencia = frecuencia
@@ -10,3 +13,24 @@ def ordenar(lista):
     lista = sorted(lista,key=lambda x: x.simbolo)
     lista = sorted(lista, key=lambda x: x.frecuencia)
     return lista
+
+def insertar(lista,nodo):
+    for i in range(len(lista)):
+        if lista[i].frecuencia > nodo.frecuencia:
+            lista.insert(i,nodo)
+            break
+        if i == len(lista)-1:
+            lista.append(nodo)
+            break
+    return lista
+
+def HuffmanTree(text):
+    if len(text) == 0:
+        return
+    
+    frecuencia = {i: text.count(i) for i in set(text)}
+
+    pq = [arbol(k,v) for k, v in frecuencia.items()]
+    heapq.heapify(pq)
+
+    
