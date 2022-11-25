9febd0110852d940dd3e34906167f67199ede174
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-11-25 11:23:42 +0100

    he creado la clase para crear el arbol y su funcion inicial

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+class arbol:
+    def __init__(self,frecuencia,simbolo) :
+        self.frecuencia = frecuencia
+        self.simbolo = simbolo
+        self.derecha = None
+        self.izquierda = None
+        self.padre = None
